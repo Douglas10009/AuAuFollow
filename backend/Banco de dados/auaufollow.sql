8421646a8fad7fe21c355757427fee675bb38722
Douglas Éverton <70817854+Douglas10009@users.noreply.github.com>
2023-11-09 09:58:40 -0300

    Update auaufollow.sql

--- backend/Banco de dados/auaufollow.sql
+++ backend/Banco de dados/auaufollow.sql
@@ -2,10 +2,11 @@ CREATE DATABASE if not EXISTS auaufollow;
 
 USE auaufollow 
 
-CREATE TABLE
-    IF NOT EXISTS clientes(
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        nome VARCHAR(200) ,
-        email VARCHAR(100),
-        senha VARCHAR(45) NOT NULL
-    );
+CREATE TABLE IF NOT EXISTS clientes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(200) ,
+    email VARCHAR(100),
+    senha VARCHAR(45) NOT NULL
+);
+
+insert into clientes (nome,email,senha) values ('adm', 'adm@gmail.com', 'adm');
